5757362d73dcd6722734836b10ab3bd0904f2fe9
hadi14250 <hadihk2k@gmail.com>
2024-09-27 12:01:48 +0400

    added bouncing ball

--- bouncingBall/bouncingBall.js
+++ bouncingBall/bouncingBall.js
@@ -1,5 +1,5 @@
 function bouncingBall(h,  bounce,  window) {
-    if (h <= 0 ||  bounce <= 0 || bounce >= 1 || window > h) return (-1);
+    if (h <= 0 ||  bounce <= 0 || bounce >= 1 || window >= h) return (-1);
     let i = 0;
     while(h > window)
     {
