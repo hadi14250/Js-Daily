1e5e8a427b0a3e724de97e650231c5ca3c5f43fb
hadi14250 <hadihk2k@gmail.com>
2024-10-06 15:59:58 +0400

    changed encryptDecrypt to encrypt to pave way for 'decrypt'

--- jsWordle/jsWordle.js
+++ jsWordle/jsWordle.js
@@ -1,27 +1,28 @@
-
 const wordle = (guess, answer) => {
-    if (guess === answer)
-        return ("g-g-g-g-g");
-    let result = ['b', 'b', 'b', 'b', 'b'];
-    let answerCount = []; 
+  if (guess === answer) return "g-g-g-g-g";
+  let result = ["b", "b", "b", "b", "b"];
+  let answerCount = [];
 
-    for (let i = 0; i < 5; i++) {
-      if (guess[i] === answer[i]) {
-        result[i] = 'g';
-      } else {
-        answerCount[answer[i]] = answerCount[answer[i]] === undefined ? 1 : answerCount[answer[i]] += 1;
-      }
+  for (let i = 0; i < 5; i++) {
+    if (guess[i] === answer[i]) {
+      result[i] = "g";
+    } else {
+      answerCount[answer[i]] =
+        answerCount[answer[i]] === undefined
+          ? 1
+          : (answerCount[answer[i]] += 1);
     }
-
-    for (let i = 0; i < 5; i++) {
-      if (result[i] === 'g') continue;
-      if (answerCount[guess[i]]) {
-        result[i] = 'y';
-        answerCount[guess[i]]--;
-      }
-    }
-  
-    return result.join('-');
   }
-  
-console.log(wordle("xxxxx", "xxxxx"))
+
+  for (let i = 0; i < 5; i++) {
+    if (result[i] === "g") continue;
+    if (answerCount[guess[i]]) {
+      result[i] = "y";
+      answerCount[guess[i]]--;
+    }
+  }
+
+  return result.join("-");
+};
+
+console.log(wordle("xxxxx", "xxxxx"));
